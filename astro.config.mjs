4ce8ed9d6ff27c0eadea35e8fde59c0608d19653
fcaravaca <100384112@alumnos.uc3m.es>
2024-03-18 17:21:46 +0100

    small fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://a2-privcomp.github.io/",
-  base: "/a2-privcomp",
+  base: "",
   integrations: [tailwind(), icon()],
 });
